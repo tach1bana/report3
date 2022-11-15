1c508d0ed6679389ade960c1ff358de91ad0a23a
Yoshiharu Habu <e205731@ie.u-ryukyu.ac.jp>
2022-11-15 21:19:56 +0900

    final answer of report3

--- app/src/main/java/jp/ac/uryukyu/ie/e205731/Hero.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e205731/Hero.java
@@ -9,10 +9,10 @@ package jp.ac.uryukyu.ie.e205731;
  * Created by tnal on 2016/11/13.
  */
 public class Hero {
-    public String name;
-    public int hitPoint;
-    public int attack;
-    public boolean dead;
+    private String name;
+    int hitPoint;
+    private int attack;
+    private boolean dead;
 
     /**
      * コンストラクタ。名前、最大HP、攻撃力を指定する。
@@ -35,7 +35,7 @@ public class Hero {
      */
     public void attack(Enemy e){
         int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, e.name, damage);
+        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, e.getName(), damage);
         e.wounded(damage);
     }
 
@@ -51,4 +51,19 @@ public class Hero {
             System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", name);
         }
     }
+
+    //以下にgetter,setterメソッドを記述
+    public String getName(){
+        return this.name;
+    }
+    public int getHitPont(){
+        return this.hitPoint;
+    }
+    public int getAttack(){
+        return this.attack;
+    }
+    public boolean getDead(){
+        return this.dead;
+    }
+
 }
